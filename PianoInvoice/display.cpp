88a4c9e8784bce97fb890563826057222fa24787
legojrp <84683257+legojrp@users.noreply.github.com>
2021-08-17 21:51:47 -0400

    1.0-alpha.0.2.2
    added more to display.cpp.

--- PianoInvoice/display.cpp
+++ PianoInvoice/display.cpp
@@ -3,15 +3,21 @@
 #include "..\..\UserDataHandledCpp\UserDataHandler.cpp"
 #include "ui_base.h"
 #include <QTableWidgetItem>
+#include <iostream>
+#include <fstream>
 void displayTo(){
 
 };
 void fromDisplay(Ui::Base *ui){
+    std::ofstream file("C:\\Users\\");
 bool xy = false;
 int x = 0;
 int y = 0;
 while (xy == false){
-    ui->tableWidget->item(x, y);
+    QTableWidgetItem* item = ui->tableWidget->item(x, y);
+    if (item->text() != nullptr){
+
+    }
 }
 };
 
